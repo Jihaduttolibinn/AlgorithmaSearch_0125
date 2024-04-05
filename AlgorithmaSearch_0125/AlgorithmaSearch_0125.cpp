5352ae81593f819deee657a27b042616ed0e8655
jihaduttolibinn <jihaduttolibin02@gmail.com>
2024-04-05 11:32:25 +0800

    step 5

--- AlgorithmaSearch_0125/AlgorithmaSearch_0125.cpp
+++ AlgorithmaSearch_0125/AlgorithmaSearch_0125.cpp
@@ -56,8 +56,21 @@ void LinearSearch()
         }
         i++; //step 4
 
-    }
 
+    
+    if (i == n) // step 5
+        cout << "\n" << item << " not found in the array\n";
+    cout << "\nNumber of comparisons: " << endl;
+
+    cout << "\nContinue search (y/n): ";
+    cin >> ch;
+
+} while ((ch == 'y') || (ch == 'Y'));
 
 }
+
+    
+
+
+
 
